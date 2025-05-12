66f649159c4fbe390bd7ccc03b2c097e6174a0dd
Shumail Kazmi <102371411+shumailkazmi@users.noreply.github.com>
2025-05-12 07:33:23 +0500

    Update app.py
    movies_dict fileupdated

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def recommend(movie):
         recommended_movies_poster.append(fetch_poster(movie_id))
     return recommended_movies, recommended_movies_poster
 
-movies_dict = pickle.load(open('movies_dict.pkl','rb'))
+movies_dict = pickle.load(open('movies_dict_.pkl','rb'))
 movies = pd.DataFrame(movies_dict)
 similarity = pickle.load(open('similarity.pkl','rb'))
 # st.title('want movies recommendations ?')
